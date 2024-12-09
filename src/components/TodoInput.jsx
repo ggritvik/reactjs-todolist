7769ae1a2c25a832c0de3b519d200fcef451b225
Ritvik Srivastava <ritviksrivastava005@gmail.com>
2024-12-09 20:58:41 +0530

    latest commit

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,14 +3,6 @@ export default function TodoInput(props) {
 
     // eslint-disable-next-line react/prop-types
     const {handleAddTodo , todoValue, setTodoValue} = props
-
-    const handleKeyDown = (e) => {
-        if (e.key === "Enter") {
-          handleAddTodo(todoValue); // Add the todo when Enter key is pressed
-          setTodoValue(''); // Clear the input field after adding
-        }
-      };
-
     return (
         <header>
             <input value={todoValue} 
