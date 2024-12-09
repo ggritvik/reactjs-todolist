47f543a91186b51bf9a4c764fe3e7b6cbfe211f1
Ritvik Srivastava <ritviksrivastava005@gmail.com>
2024-12-09 20:45:08 +0530

    add todo with enter key

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -4,15 +4,27 @@ export default function TodoInput(props) {
     // eslint-disable-next-line react/prop-types
     const {handleAddTodo , todoValue, setTodoValue} = props
 
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+          handleAddTodo(todoValue); // Add the todo when Enter key is pressed
+          setTodoValue(''); // Clear the input field after adding
+        }
+      };
+
     return (
         <header>
             <input value={todoValue} 
             onChange={(e) => {
                 setTodoValue(e.target.value)}} 
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      handleAddTodo(todoValue); // Add the todo when Enter key is pressed
+                      setTodoValue(''); // Clear the input field after adding
+                    }}}
                 placeholder="What needs to be done?..." />
             
             <button onClick={() => {
-                handleAddTodo(todoValue)
+                handleAddTodo(todoValue)    
                 setTodoValue('')
                 }}>Add
             </button>
